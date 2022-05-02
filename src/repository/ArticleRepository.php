43b2dff7bdba43bd9a01671256460b32a27ae8fc
Marty <martyrabord@yahoo.fr>
2022-05-02 16:22:59 +0200

    maj article

--- src/repository/ArticleRepository.php
+++ src/repository/ArticleRepository.php
@@ -2,12 +2,36 @@
 
 class ArticleRepository extends Repository
 {
-    CONST ARTICLE_TABLE="CREATE TABLE IF NOT EXISTS 
+    CONST ARTICLE_TABLE="CREATE TABLE IF NOT EXISTS Article ( 
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     content TEXT NOT NULL,
     title VARCHAR(255),
-    published_date DATETIME";
+    published_date DATETIME);";
 
 
-    CONST ARTICLE_INSERT="INSERT INTO table_name(colunm_name1, colunm_name2,...) VALUE(VALUE11, VALUE12), (VALUE21, VALUE22),...;";
+    CONST ARTICLE_INSERT="INSERT INTO Article(content, title) VALUE('titre','text'), ('varchar', 'datetime');";
+
+
+    private $table;
+
+   function __construct($table){
+        $this->table= $table;
+    }
+
+
+    
+    public function findAll():array
+    {
+        $tableau=[];
+
+        $query = "('SELECT * from Article')";
+        $this->createTableIfNotExistes($this->table,self::ARTICLE_TABLE,self::ARTICLE_INSERT);
+
+        $this->executeQuery($this->query);
+
+        
+
+        fetchAll( fetch_class, "article")
+
+    }
 }
